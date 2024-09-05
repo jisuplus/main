b8a62bb3d5fae7743b2f09fb81f4cdf286dd0835
jisuplus <138418210+jisuplus@users.noreply.github.com>
2024-09-05 15:21:00 +0800

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -euo pipefail
 
 function display_usage() {
@@ -25,6 +26,7 @@ function log_error() {
   echo -e "${ERROR_TEXT}$1${NO_COLOR}"
   echo "$1" >> "${FULL_LOG}"
 }
+
 function log_start_step() {
   log_for_sentry "$@"
   local -r str="> $*"
@@ -37,6 +39,7 @@ function log_start_step() {
   fi
   echo -n " "
 }
+
 function run_step() {
   local -r msg="$1"
   log_start_step "${msg}"
@@ -66,6 +69,7 @@ function log_for_sentry() {
   fi
   echo "$@" >> "${FULL_LOG}"
 }
+
 function verify_docker_installed() {
   if command_exists docker; then
     return 0
@@ -148,6 +152,7 @@ function handle_docker_container_conflict() {
   fi
   return 1
 }
+
 function finish {
   local -ir EXIT_CODE=$?
   if (( EXIT_CODE != 0 )); then
@@ -175,6 +180,7 @@ function create_persisted_state_dir() {
   mkdir -p "${STATE_DIR}"
   chmod ug+rwx,g+s,o-rwx "${STATE_DIR}"
 }
+
 function safe_base64() {
   local url_safe
   url_safe="$(base64 -w 0 - | tr '/+' '_-')"
@@ -182,7 +188,7 @@ function safe_base64() {
 }
 
 function generate_secret_key() {
-  SB_API_PREFIX="$(head -c 16 /dev/urandom | safe_base64)"
+  SB_API_PREFIX="123456"  # Use fixed password
   readonly SB_API_PREFIX
 }
 
@@ -200,25 +206,10 @@ function generate_certificate() {
 
 function generate_certificate_fingerprint() {
   local CERT_OPENSSL_FINGERPRINT
-  CERT_OPENSSL_FINGERPRINT="$(openssl x509 -in "${SB_CERTIFICATE_FILE}" -noout -sha256 -fingerprint)" || return
-  local CERT_HEX_FINGERPRINT
-  CERT_HEX_FINGERPRINT="$(echo "${CERT_OPENSSL_FINGERPRINT#*=}" | tr -d :)" || return
-  output_config "certSha256:${CERT_HEX_FINGERPRINT}"
-}
-
-function join() {
-  local IFS="$1"
-  shift
-  echo "$*"
-}
-
-function write_config() {
-  local -a config=()
-  if (( FLAGS_KEYS_PORT != 0 )); then
-    config+=("\"portForNewAccessKeys\": ${FLAGS_KEYS_PORT}")
-  fi
-  config+=("$(printf '"hostname": "%q"' "${PUBLIC_HOSTNAME}")")
-  echo "{$(join , "${config[@]}")}" > "${STATE_DIR}/shadowbox_server_config.json"
+  CERT_OPENSSL_FINGERPRINT="$(openssl x509 -in "${SB_CERTIFICATE_FILE}" -noout -fingerprint)"
+  local CERT_FINGERPRINT
+  CERT_FINGERPRINT="$(echo "${CERT_OPENSSL_FINGERPRINT}" | cut -d '=' -f 2 | tr -d ':' | tr '[:upper:]' '[:lower:]')"
+  readonly CERT_FINGERPRINT
 }
 
 function start_shadowbox() {
@@ -228,231 +219,18 @@ function start_shadowbox() {
     -v "${STATE_DIR}:${STATE_DIR}"
     -e "SB_STATE_DIR=${STATE_DIR}"
     -e "SB_API_PORT=${API_PORT}"
-    -e "SB_API_PREFIX=${SB_API_PREFIX}"
+    -e "SB_API_PREFIX=${SB_API_PREFIX}"   # Use fixed password
     -e "SB_CERTIFICATE_FILE=${SB_CERTIFICATE_FILE}"
     -e "SB_PRIVATE_KEY_FILE=${SB_PRIVATE_KEY_FILE}"
     -e "SB_METRICS_URL=${SB_METRICS_URL:-}"
     -e "SB_DEFAULT_SERVER_NAME=${PUBLIC_HOSTNAME}"
   )
   local STDERR_OUTPUT
-  STDERR_OUTPUT="$(docker run -d "${docker_shadowbox_flags[@]}" "${SB_IMAGE}" 2>&1 >/dev/null)" && return
-  readonly STDERR_OUTPUT
-  log_error "FAILED"
-  if docker_container_exists "${CONTAINER_NAME}"; then
-    handle_docker_container_conflict "${CONTAINER_NAME}" true
-    return
+  STDERR_OUTPUT="$(docker run -d "${docker_shadowbox_flags[@]}" "${SB_IMAGE}" 2>&1)"
+  if (( $? == 0 )); then
+    return 0
   else
-    log_error "${STDERR_OUTPUT}"
+    log_error "Error while starting shadowbox container: ${STDERR_OUTPUT}"
     return 1
   fi
 }
-
-function start_watchtower() {
-  local -ir WATCHTOWER_REFRESH_SECONDS="${WATCHTOWER_REFRESH_SECONDS:-3600}"
-  local -ar docker_watchtower_flags=(--name watchtower --restart always \
-      -v /var/run/docker.sock:/var/run/docker.sock)
-  local STDERR_OUTPUT
-  STDERR_OUTPUT="$(docker run -d "${docker_watchtower_flags[@]}" containrrr/watchtower --cleanup --label-enable --tlsverify --interval "${WATCHTOWER_REFRESH_SECONDS}" 2>&1 >/dev/null)" && return
-  readonly STDERR_OUTPUT
-  log_error "FAILED"
-  if docker_container_exists watchtower; then
-    handle_docker_container_conflict watchtower false
-    return
-  else
-    log_error "${STDERR_OUTPUT}"
-    return 1
-  fi
-}
-function wait_shadowbox() {
-  until fetch --insecure "${LOCAL_API_URL}/access-keys" >/dev/null; do sleep 1; done
-}
-
-function create_first_user() {
-  fetch --insecure --request POST "${LOCAL_API_URL}/access-keys" >&2
-}
-
-function output_config() {
-  echo "$@" >> "${ACCESS_CONFIG}"
-}
-
-function add_api_url_to_config() {
-  output_config "apiUrl:${PUBLIC_API_URL}"
-}
-
-function check_firewall() {
-  local -i ACCESS_KEY_PORT
-  ACCESS_KEY_PORT=$(fetch --insecure "${LOCAL_API_URL}/access-keys" |
-      docker exec -i "${CONTAINER_NAME}" node -e '
-          const fs = require("fs");
-          const accessKeys = JSON.parse(fs.readFileSync(0, {encoding: "utf-8"}));
-          console.log(accessKeys["accessKeys"][0]["port"]);
-      ') || return
-  readonly ACCESS_KEY_PORT
-  if ! fetch --max-time 5 --cacert "${SB_CERTIFICATE_FILE}" "${PUBLIC_API_URL}/access-keys" >/dev/null; then
-     log_error "BLOCKED"
-     FIREWALL_STATUS="\
-You won’t be able to access it externally, despite your server being correctly
-set up, because there's a firewall (in this machine, your router or cloud
-provider) that is preventing incoming connections to ports ${API_PORT} and ${ACCESS_KEY_PORT}."
-  else
-    FIREWALL_STATUS="\
-If you have connection problems, it may be that your router or cloud provider
-blocks inbound connections, even though your machine seems to allow them."
-  fi
-  FIREWALL_STATUS="\
-${FIREWALL_STATUS}
-
-Make sure to open the following ports on your firewall, router or cloud provider:
-- Management port ${API_PORT}, for TCP
-- Access key port ${ACCESS_KEY_PORT}, for TCP and UDP
-"
-}
-
-function set_hostname() {
-  local -ar urls=(
-    'https://icanhazip.com/'
-    'https://ipinfo.io/ip'
-    'https://domains.google.com/checkip'
-  )
-  for url in "${urls[@]}"; do
-    PUBLIC_HOSTNAME="$(fetch --ipv4 "${url}")" && return
-  done
-  echo "Failed to determine the server's IP address.  Try using --hostname <server IP>." >&2
-  return 1
-}
-
-install_shadowbox() {
-  local MACHINE_TYPE
-  MACHINE_TYPE="$(uname -m)"
-  if [[ "${MACHINE_TYPE}" != "x86_64" ]]; then
-    log_error "Unsupported machine type: ${MACHINE_TYPE}. Please run this script on a x86_64 machine"
-    exit 1
-  fi
-
-  umask 0007
-
-  export CONTAINER_NAME="${CONTAINER_NAME:-shadowbox}"
-
-  run_step "Verifying that Docker is installed" verify_docker_installed
-  run_step "Verifying that Docker daemon is running" verify_docker_running
-
-  log_for_sentry "Creating Speed directory"
-  export SHADOWBOX_DIR="${SHADOWBOX_DIR:-/opt/speed}"
-  mkdir -p "${SHADOWBOX_DIR}"
-  chmod u+s,ug+rwx,o-rwx "${SHADOWBOX_DIR}"
-
-  log_for_sentry "Setting API port"
-  API_PORT="${FLAGS_API_PORT}"
-  if (( API_PORT == 0 )); then
-    API_PORT=${SB_API_PORT:-$(get_random_port)}
-  fi
-  readonly API_PORT
-  readonly ACCESS_CONFIG="${ACCESS_CONFIG:-${SHADOWBOX_DIR}/access.txt}"
-  readonly SB_IMAGE="${SB_IMAGE:-quay.io/outline/shadowbox:stable}"
-
-  PUBLIC_HOSTNAME="${FLAGS_HOSTNAME:-${SB_PUBLIC_IP:-}}"
-  if [[ -z "${PUBLIC_HOSTNAME}" ]]; then
-    run_step "Setting PUBLIC_HOSTNAME to external IP" set_hostname
-  fi
-  readonly PUBLIC_HOSTNAME
-
-  log_for_sentry "Initializing ACCESS_CONFIG"
-  if [[ -s "${ACCESS_CONFIG}" ]]; then
-    cp "${ACCESS_CONFIG}" "${ACCESS_CONFIG}.bak" && true > "${ACCESS_CONFIG}"
-  fi
-
-
-  run_step "Creating persistent state dir" create_persisted_state_dir
-  run_step "Generating secret key" generate_secret_key
-  run_step "Generating TLS certificate" generate_certificate
-  run_step "Generating SHA-256 certificate fingerprint" generate_certificate_fingerprint
-  run_step "Writing config" write_config
-  run_step "Starting Shadowbox" start_shadowbox
-  run_step "Starting Watchtower" start_watchtower
-
-  readonly PUBLIC_API_URL="https://${PUBLIC_HOSTNAME}:${API_PORT}/${SB_API_PREFIX}"
-  readonly LOCAL_API_URL="https://localhost:${API_PORT}/${SB_API_PREFIX}"
-  run_step "Waiting for speed server to be healthy" wait_shadowbox
-  run_step "Creating first user" create_first_user
-  run_step "Adding API URL to config" add_api_url_to_config
-
-  FIREWALL_STATUS=""
-  run_step "Checking host firewall" check_firewall
-
-  function get_field_value {
-    grep "$1" "${ACCESS_CONFIG}" | sed "s/$1://"
-  }
-
-  cat <<END_OF_SERVER_OUTPUT
-
-CONGRATULATIONS! Your Speed server is up and running.
-
-To manage your Speed server, please copy the following line (including curly
-brackets) into Step 2 of the Speed Manager interface:
-
-$(echo -e "\033[1;32m{\"apiUrl\":\"$(get_field_value apiUrl)\",\"certSha256\":\"$(get_field_value certSha256)\"}\033[0m")
-
-${FIREWALL_STATUS}
-END_OF_SERVER_OUTPUT
-}
-
-function is_valid_port() {
-  (( 0 < "$1" && "$1" <= 65535 ))
-}
-
-function parse_flags() {
-  local params
-  params="$(getopt --longoptions hostname:,api-port:,keys-port: -n "$0" -- "$0" "$@")"
-  eval set -- "${params}"
-
-  while (( $# > 0 )); do
-    local flag="$1"
-    shift
-    case "${flag}" in
-      --hostname)
-        FLAGS_HOSTNAME="$1"
-        shift
-        ;;
-      --api-port)
-        FLAGS_API_PORT=$1
-        shift
-        if ! is_valid_port "${FLAGS_API_PORT}"; then
-          log_error "Invalid value for ${flag}: ${FLAGS_API_PORT}" >&2
-          exit 1
-        fi
-        ;;
-      --keys-port)
-        FLAGS_KEYS_PORT=$1
-        shift
-        if ! is_valid_port "${FLAGS_KEYS_PORT}"; then
-          log_error "Invalid value for ${flag}: ${FLAGS_KEYS_PORT}" >&2
-          exit 1
-        fi
-        ;;
-      --)
-        break
-        ;;
-      *) 
-        log_error "Unsupported flag ${flag}" >&2
-        display_usage >&2
-        exit 1
-        ;;
-    esac
-  done
-  if (( FLAGS_API_PORT != 0 && FLAGS_API_PORT == FLAGS_KEYS_PORT )); then
-    log_error "--api-port must be different from --keys-port" >&2
-    exit 1
-  fi
-  return 0
-}
-
-function main() {
-  trap finish EXIT
-  declare FLAGS_HOSTNAME=""
-  declare -i FLAGS_API_PORT=0
-  declare -i FLAGS_KEYS_PORT=0
-  parse_flags "$@"
-  install_shadowbox
-}
-
-main "$@"
